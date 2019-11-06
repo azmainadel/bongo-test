737a938e94e65fc443229e1c241e1dac6f682ebf
Azmain Adel <azmainadel47@gmail.com>
2019-11-06 18:46:54 +0600

    Add videoview

--- problem-three/BongoVideoPlayer/app/src/main/java/com/xyntherys/bongovideoplayer/MainActivity.java
+++ problem-three/BongoVideoPlayer/app/src/main/java/com/xyntherys/bongovideoplayer/MainActivity.java
@@ -1,13 +1,76 @@
 package com.xyntherys.bongovideoplayer;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String VIDEO_SAMPLE = "sample_video";
+    private VideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+    }
+
+    private void initView() {
+        mVideoView = findViewById(R.id.videoview);
+    }
+
+    private void initPlayer() {
+        Uri videoUri = getMedia(VIDEO_SAMPLE);
+        mVideoView.setVideoURI(videoUri);
+
+        mVideoView.start();
+    }
+
+    private void stopPlayer(){
+        mVideoView.stopPlayback();
+    }
+
+    private Uri getMedia(String mediaName) {
+        return Uri.parse("android.resource://" + getPackageName() +
+                "/raw/" + mediaName);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        initPlayer();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        stopPlayer();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            mVideoView.pause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
---
+++ problem-three/BongoVideoPlayer/app/src/main/java/com/xyntherys/bongovideoplayer/model/MediaFile.java
@@ -0,0 +1,10 @@
+package com.xyntherys.bongovideoplayer.model;
+
+//
+// Created by Azmain Adel on 11/6/19.
+// Copyright (c) 2019 Field Buzz. All rights reserved.
+//
+
+public class MediaFile {
+
+}
